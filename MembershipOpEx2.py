7b987f5a47fdf8f7ad33bfb98807c2f94413dfee
Akash Ohalkar <akashohalkar300@gmail.com>
2025-09-01 17:08:52 +0530

    Create MembershipOpEx2.py

---
+++ MembershipOpEx2.py
@@ -0,0 +1,11 @@
+a = "PYTHON"
+print("_"*20)
+
+b = "p" not in a
+c = "yt" in a
+d = "hon" in a
+e = "P" in a
+f = "YT" not in a
+g = "HON" in a
+print(a,"\n",b,"\n",c,"\n",d,"\n",e,"\n",f,"\n",g)
+
