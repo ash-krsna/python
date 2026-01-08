d37c6c177665d777bb13185395ce58c2cb992569
Akash Ohalkar <akashohalkar300@gmail.com>
2026-01-08 20:08:07 +0530

    Describe your changes

--- HomeWork/question3.py
+++ HomeWork/question3.py
@@ -3,7 +3,7 @@ input: [3,0,1]
 output: 2  
 nums = [3,0,1]
 n = len(nums)
-total = n * (n + 1) // 2
+total = n * (n + 1) // 2n  asdfj
 sum_of_nums = sum(nums)
 missing_number = total - sum_of_nums
 print(missing_number)
