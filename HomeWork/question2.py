be8101b95474ec3d13d85750e3e170052e4613a5
Akash Ohalkar <akashohalkar300@gmail.com>
2026-01-15 13:26:06 +0530

    Describe your changes

---
+++ HomeWork/question2.py
@@ -0,0 +1,11 @@
+nums = [0, 1, 0, 3, 12]
+a = sorted(nums)
+print(a)
+c = list(reversed(a))
+print(c)
+cd D:\Python
+git add .
+git commit -m "Describe your changes"
+git pull origin main --rebase
+git push origin main
+                
