2da1e5b3c03877b069b591340002104d6db28d19
Akash Ohalkar <akashohalkar300@gmail.com>
2026-06-25 18:28:21 +0530

    Fix output message for maximum value comparison

--- HomeWork/H-W-TernaryEx1.py
+++ HomeWork/H-W-TernaryEx1.py
@@ -3,3 +3,4 @@ a = float(input("Enter First Value:"))
 b = float(input("Enter Second Value:"))
 maxval = "a is greater" if (a > b) else "b is greater" if (a != b) else "Both are equal"
 print(maxval)
+ 
