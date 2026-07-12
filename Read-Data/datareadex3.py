0aeef45ac8627dc848dd24d443df903d607c55c2
Akash Ohalkar <akashohalkar300@gmail.com>
2026-07-12 08:55:34 +0530

    Update datareadex3.py

--- Read-Data/datareadex3.py
+++ Read-Data/datareadex3.py
@@ -6,7 +6,7 @@ a = float(x)
 b = float(y)
 #find product
 c = a * b
-#display the result
+#display the result 
 print("-"*50)
 print("\t\tVal of a = {}".format(a))
 print("\t\tVal of b = {}".format(b))
